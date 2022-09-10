13a4dd689645a395e94ea1774146053bf29450e5
Kritagya Singh Chouhan <kritagya2022@gmail.com>
2022-09-10 22:30:17 +0530

    updated blogs and projects page of the website

--- assets/js/data.js
+++ assets/js/data.js
@@ -25,19 +25,6 @@ const blogs = [
 const projects = [ 
     {
         id: 'p0',
-        heading: 'Tribute Page',
-        description: `A website created as a tribute to Mahendra Singh Dhoni, a living legend. It contains all the information on accomplishments, statistics, and records. The website skillfully presents the challenging numbers in the most appealing graphs and charts.`,
-        techList: {
-            tl1: 'HTML',
-            tl2: 'CSS',
-            tl3: 'Javascript',
-            tl4: 'VS Code'
-        },
-        githubLink: 'https://github.com/kritagya20',
-        image: './assets/project-archieves/tribute-page-demo-image.jpg'
-    },
-    {
-        id: 'p1',
         heading: 'Weather App',
         description: `An online application that shows the most pertinent data currently available on the weather across world. The website forecasts the majority of credible reports using the Open Weather API. `,
         techList: {
@@ -50,8 +37,8 @@ const projects = [
         image: './assets/project-archieves/weather-app-demo-image.jpg'
     },
     {
-        id: 'p2',
-        heading: 'Youtube Clone',
+        id: 'p1',
+        heading: 'Youtube UI Clone',
         description: `A website for rendering videos that adopts the YouTube user interface. To display the video list, the website uses the YouTube API. Additionally, a search bar that works is included. `,
         techList: {
             tl1: 'HTML',
@@ -63,7 +50,7 @@ const projects = [
         image: './assets/project-archieves/youtube-clone-demo-image.jpg'
     },
     {
-        id: 'p3',
+        id: 'p2',
         heading: 'Dashboard',
         description: `Static UI dashboard page for a demo business with intricate patterns and data visualisation. The Page is upadated using dynamic data insertion via JavaScript.`,
         techList: {
@@ -76,7 +63,7 @@ const projects = [
         image: './assets/project-archieves/dashboard-demo-image.jpg'
     },
     {
-        id: 'p4',
+        id: 'p3',
         link: '#',
         heading: 'School Website',
         description: `The website's navigation bar and footer are both useful. There are four pages on the website: "home," "about," "courses," and "contact." The pages have all been designed with full responsiveness in mind. `,
@@ -89,6 +76,19 @@ const projects = [
         githubLink: 'https://github.com/kritagya20/school-website',
         externalLink: '#',
         image: './assets/project-archieves/school-website-demo-image.jpg'
+    },
+    {
+        id: 'p4',
+        heading: 'Tribute Page',
+        description: `A website created as a tribute to Mahendra Singh Dhoni, a living legend. It contains all the information on accomplishments, statistics, and records. The website skillfully presents the challenging numbers in the most appealing graphs and charts.`,
+        techList: {
+            tl1: 'HTML',
+            tl2: 'CSS',
+            tl3: 'Javascript',
+            tl4: 'VS Code'
+        },
+        githubLink: 'https://github.com/kritagya20',
+        image: './assets/project-archieves/tribute-page-demo-image.jpg'
     }
 ];
 
