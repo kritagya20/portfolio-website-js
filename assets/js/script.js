f8ccd8b81f915a79c4ac01795756255d31badf3e
Kritagya Singh Chouhan <kritagya2022@gmail.com>
2022-09-25 13:38:48 +0530

    development of 24-09-2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,29 +199,26 @@ try {
       firstName: firstName.value,
       lastName: lastName.value,
       email: email.value,
-      message: message.value
-    }
+      message: message.value,
+    };
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function (){
-      console.log(xhr.responseText);
-      if(xhr.responseText == 'success') {
-        alert ('Email sent');
-        firstName.value = '';
-        lastName.value = '';
-        email.value = '';
-        message.value = '';
-      } else {
-        console.log('error');
-      }
-    }
+    const serivce_id = "service_ep5wsq2";
+    const template_id = "template_9qk7m26";
 
-    xhr.send(JSON.stringify(formData));
+    emailjs
+      .send(serivce_id, template_id, formData)
+      .then((res) => {
+        alert("Email sent");
+        console.log(res);
+        firstName.value = "";
+        lastName.value = "";
+        email.value = "";
+        message.value = "";
+      })
+      .catch((e) => console.log(`Error caught in submitting form: ${e}`));
   });
 } catch (e) {
-  console.log(`Error caught in form validation: ${e}`);
+  console.log(`Error caught in form section: ${e}`);
 }
 
 //INTERSECTION OBSERVER
